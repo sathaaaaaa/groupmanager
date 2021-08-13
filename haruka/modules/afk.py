b65eef38b26b5df5e7da361212ad714f7d130ecb
sathaaaaaa <85862925+sathaaaaaa@users.noreply.github.com>
2021-08-13 21:38:53 +0530

    afk.py

--- haruka/modules/afk.py
+++ haruka/modules/afk.py
@@ -40,7 +40,7 @@ def no_longer_afk(bot: Bot, update: Update):
     res = sql.rm_afk(user.id)
     if res:
         firstname = update.effective_user.first_name
-        update.effective_message.reply_text(tld(chat.id, f"{firstname} is no longer AFK!"))
+        update.effective_message.reply_text(tld(chat.id, f"{firstname} is no longer fucking AFK!"))
 
 
 @run_async
